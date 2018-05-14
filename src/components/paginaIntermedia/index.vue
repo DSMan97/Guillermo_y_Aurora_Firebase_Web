c3ffa38c83e4b98df9509a502d4b4837ee7fa6f4
Aurora Ruelas <karyna.aurora@gmail.com>
2018-05-14 09:46:14 +0200

    agregando otro componente para pagina intermedia

---
+++ src/components/paginaIntermedia/index.vue
@@ -0,0 +1,4 @@
+<template src="./paginaIntermedia.component.html"></template>
+<script src="./paginaIntermedia.component.js"></script>
+<style src="./paginaIntermedia.component.scss" scoped lang="scss"></style>
+
---
+++ src/components/paginaIntermedia/paginaIntermedia.component.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'pagina-intermedia',
+  components: {}, 
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
