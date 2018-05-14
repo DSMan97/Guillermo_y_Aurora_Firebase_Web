6d8057bc10c2ee1323ce0837286476cf83deeca6
Aurora Ruelas <karyna.aurora@gmail.com>
2018-05-14 09:09:21 +0200

    prueba

--- src/components/Principal/Principal.component.js
+++ src/components/Principal/Principal.component.js
@@ -3,7 +3,7 @@ import LoginRegistro from '@/components/LoginRegistro'
 export default {
   name: 'Principal',
   components: {
-'loginregistro':LoginRegistro
+LoginRegistro : 'loginregistro'
   },
   props: [],
   data () {
